e37d56aad9bf982431bffa8050a66bc046516626
Ethan Henley <ethanchenley@gmail.com>
2024-06-18 21:11:58 -0400

    Initial commit

---
+++ brewtilities.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+brew install \
+  ack \
+  chrome-cli \
+  docker \
+  docker-compose \
+  ffmpeg \
+  mackup \
+  mas \
+  nano \
+  netcat \
+  netdata \
+  nginx \
+  node \
+  python \
+  telnet \
+  wget \
+  whois \
+  yt-dlp \
+  switchaudio-osx \
+  yarn
